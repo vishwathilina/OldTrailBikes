4045c577e908a0b3cdef42d912200921d1f61c31
vishwathilina <vishwathilina@outlook.com>
2026-05-24 09:26:00 +0530

    chore(backend): add linting, formatting, and gitignore

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    es2022: true,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+  ignorePatterns: ['dist', 'node_modules', 'prisma/migrations'],
+};
